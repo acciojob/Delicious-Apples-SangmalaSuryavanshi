fc9de436b930ddaa94d680d9dd4513c5af048fd0
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-10-02 23:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ cy.get('.photo-main .photo-main-img');
 cy.get('.photo-album li img');
 cy.get('.price h2');
 cy.get('.variant li img');
+
 cy.get('body').then((body) => {
   if (body.find('.photo-album>ul>li>img').length > 0) {  
     cy.get('.photo-album>ul>li>img');
   }
 });
+cy.get('.photo-album>ul>li>img', { timeout: 10000 });
